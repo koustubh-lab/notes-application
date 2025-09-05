f9cce65eadf3d4dfa69c7c32a782c723f1b57abb
Koustubh Karande <karandekoustubh9@gmail.com>
2025-09-05 21:46:29 +0530

    More Changes in MvcConfigurer

--- backend/src/main/java/com/koustubh/notes_application/security/CustomWebMvcConfigurer.java
+++ backend/src/main/java/com/koustubh/notes_application/security/CustomWebMvcConfigurer.java
@@ -14,7 +14,7 @@ public class CustomWebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // adjust path as needed
-                        .allowedOrigins("https://notes-application-yow6.onrender.com") // frontend URL
+                        .allowedOrigins("https://notes-application-fawn.vercel.app/") // frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
